89822538d20c2282e39c9732771d6820749b731b
NCR666 <claudio.raharison@gmail.com>
2026-01-02 17:46:24 +0300

    test liens

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const isBuild = command === 'build';
-  return {
-    base: isBuild ? '/portfolio/' : '/',
-    plugins: [react()],
-  }
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
 })
