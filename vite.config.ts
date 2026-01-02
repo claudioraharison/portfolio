ffb1b0bcf12815fe1e55ff11b185916806abfabb
NCR666 <claudio.raharison@gmail.com>
2026-01-02 17:33:42 +0300

    liens imprevu

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+  return {
+    base: isBuild ? '/portfolio/' : '/',
+    plugins: [react()],
+  }
 })
